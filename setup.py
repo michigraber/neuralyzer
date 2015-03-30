2ab729780cfa9aaf2634be9244c965bcb12fdcee
mhg <michigraber@gmail.com>
2015-03-30 11:25:29 +0900

    switched to setuptools

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
-#!/usr/bin/env python
 
-from distutils.core import setup
+import os
+from setuptools import setup
 
-setup(name='neuralyzer',
-      version='0.0.1',
-      description='A neuroscience imaging toolset.',
-      author='Michael Graber',
-      author_email='michael.graber@gmail.com',
-      packages=[
-          'neuralyzer',
-          'neuralyzer.io',
-          'neuralyzer.io.plugins',
-          'neuralyzer.utils',
-          ],
-      scripts=['scripts/build_tiff_stack.py',]
-     )
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "neuralyzer",
+    version = "0.0.1",
+    author = "Michael Graber",
+    author_email = "michigraber@gmail.com",
+    description = ("A neuroscience data analysis tool suite."),
+    license = "MIT",
+    keywords = "neuroscience, data analysis, image processing",
+    packages=[
+        'neuralyzer',
+        'neuralyzer.io',
+        'neuralyzer.io.plugins',
+        'neuralyzer.utils',
+        ],
+    scripts=['scripts/build_tiff_stack.py',],
+    long_description=read('README.rst'),
+)
+
