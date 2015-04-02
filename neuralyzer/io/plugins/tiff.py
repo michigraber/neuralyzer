faec9e30dcf961db8ab247fcaa9321b1c9c55826
mhg <michigraber@gmail.com>
2015-04-02 15:56:52 +0900

    allow for library (tifffile / PIL) choice

--- neuralyzer/io/plugins/tiff.py
+++ neuralyzer/io/plugins/tiff.py
@@ -5,23 +5,59 @@ from neuralyzer.io.loader import LoaderTemplate
 
 FILE_EXTENSIONS = ('tiff', 'tif', )
 
+DEFAULT_LIBRARY = 'PIL'
+
 class Loader(LoaderTemplate):
     
-    # TODO : might wanna implement here a general FileHandler
-
     @staticmethod
-    def get_data(filepath):
+    def get_data(filepath, library=DEFAULT_LIBRARY):
 
-        try:
-            import numpy as np
-            from PIL import Image
-            return np.array(Image.open(filepath))
-        except ImportError:
-            pass
+        if library == 'PIL':
+            try:
+                return get_data_PIL(filepath)
+            except:
+                raise 
 
-        try:
-            from tifffile import TiffFile
-            mytiff = TiffFile(filepath)
-            return mytiff.asarray()
-        except:
-            raise
+        elif library == 'tifffile':
+            try:
+                return get_data_tifffile(filepath)
+            except:
+                raise
+
+        else:
+            raise ValueError('No load method implemented for library %s' % library)
+
+
+# reading files using PIL
+# -----------------------------------------------------------------------------
+
+PILMode2NPdtype = {
+        'I;16': 'uint16',
+        }
+
+def get_data_PIL(filepath):
+    import numpy as np
+    from PIL import Image, ImageSequence
+
+    img = Image.open(filepath)
+    try:
+        dtype = PILMode2NPdtype[img.mode]
+    except KeyError:
+        raise NotImplementedError(('The handling of tif files with PIL'
+                ' mode "%s" is currently not supported.') % img.mode)
+
+    framedata = [np.array(frame)
+            for frame in ImageSequence.Iterator(img)]
+    image = np.array(framedata)
+    img.close()
+    return image 
+
+
+# reading files using tifffile
+# -----------------------------------------------------------------------------
+
+def get_data_tifffile(filepath):
+    ''' reads a tifffile using the tifffile library '''
+    from tifffile import TiffFile
+    mytiff = TiffFile(filepath)
+    return mytiff.asarray()
