0c9ee9bb081f7a33d2cae3788446959690be3793
mhg <michigraber@gmail.com>
2015-06-11 11:26:26 +0900

    smff first working version

--- neuralyzer/im/smff/model.py
+++ neuralyzer/im/smff/model.py
@@ -11,229 +11,200 @@ by Pnevmatikakis et al. 2014
 
 import numpy as np
 
+from sklearn import linear_model 
+
 from neuralyzer.im import nmf
+from neuralyzer import log
 
-#from neuralyzer.utils import log
-
-
-
-
-# MODEL INITIALIZATION CODE
-# -----------------------------------------------------------------------------
-
-def _init_model(Y, gaussian_blur_memmap, num_components=1, ws=41):
-    '''
-    
-    Y: the data of form [Y] = d x T
-    gaussian_blur_memmap : a numpy memory map to a gaussian blur matrix
-
-    num_components : the number of components (neurons) we aim for
-
-    WARNING !! we assume quadratic image shape
-
-    '''
-
-    TINY_POSITIVE_NUMBER = np.finfo(np.float).tiny 
-
-    d, T = Y.shape
-
-    #mediandata = np.median(Y, axis=1)
-    #R = Y - np.tile(mediandata, (Y.shape[1], 1)).T
-    R = Y.copy()
-    D = gaussian_blur_memmap
-
-    ims = int(np.sqrt(d))
-    # in case we would like to make it an input argument at some point ..
-    ims = (ims, ims)
-
-    A, C = [], []
-
-    for k in range(num_components):
-
-        rho = np.dot(D, R)
-        rhomax = rho.max(axis=1)
-        wcent = np.argmax(rhomax)
-        wcent = (np.mod(wcent, ims[0]), int(wcent/ims[0]))
-        wmask = window_mask(wcent, ims, ws).flatten()
-        Rw = R[wmask, :]
-        H_init = rhomax[wmask].flatten()
-        W, H = _nmf_lars_greedy_init(Rw.T, H_init, iterations=30, normalize=False, alpha=0.1)
-        ak = np.zeros(R.shape[0])
-        ak.flat[wmask] = H
-        
-        A.append(ak/ak.sum()) # normalize
-        C.append(W.clip(TINY_POSITIVE_NUMBER, np.inf))
-        R[wmask, :] = (R[wmask, :] - np.dot(W, H.T).T).clip(TINY_POSITIVE_NUMBER, np.inf)
-
-    # initialize background trace + signal
-    # get better init estimate for background!!
-    f, b = _nmf_lars_greedy_init(R.T, 'random', k=1, iterations=30, normalize=False, alpha=1.)
-    f = f/f.sum()
-
-    C = np.array(C).reshape(num_components, T) # will be of shape k x T
-    A = np.array(A).T # will be of shape d x k
-
-    return A, C, b, f 
+logger = log.get_logger()
 
 
-def _nmf_lars_greedy_init(V, H_init, k=None, iterations=30, **kwargs):
-    '''
-    V = WH : V, W, H >= 0
+TINY_POSITIVE_NUMBER = np.finfo(np.float).tiny 
 
-    V.shape = (m, n)
-    W.shape = (m, k)
-    H.shape = (k, n)
-    '''
-    from sklearn.linear_model import LassoLars 
-    m, n = V.shape
-    if H_init == 'random':
-        H = np.random.rand(n, k)
-    else:
-        H = H_init
-        if H.ndim == 1:
-            H.shape  += (1,) 
-
-    # Perform 'alternating' minimization.
-    for iter_num in range(iterations):
-        lB = LassoLars(positive=True, max_iter=200, **kwargs)
-        lB.fit(H, V.T)
-        W = lB.coef_
-
-
-
-
-
-        lA = LassoLars(positive=True, max_iter=200, **kwargs)
-        lA.fit(W, V)
-        H = lA.coef_
-
-    return W, H
-
-
-def window_mask(cent, ims, ws):
-    ''' symmetric and odd window sizes only '''
-    w = np.zeros(ims, dtype='bool')
-    if ws == 0:
-        return w
-    if not(ws % 2):
-        raise ValueError('ws has to be odd.')
-    ws = int(ws/2)
-    xl = np.array([cent[1]-ws, cent[1]+ws+1]).clip(0, ims[1])
-    yl = np.array([cent[0]-ws, cent[0]+ws+1]).clip(0, ims[0])
-    w[xl[0]:xl[1], yl[0]:yl[1]] = True
-    return w
-
-
-
-def _gauss_kernel(size, var=None):    
-    if type(size) == int:
-        size = (size, size)
-    else:
-        raise ValueError('We only provide symmetric kernels with sizes specified by an int.')
-    if not np.mod(size[0],2) or not np.mod(size[1],2):
-        raise ValueError('The size of the Kernel has to be odd.')
-    s = (int(size[0]/2.), int(size[0]/2.))
-    if var is None: v = s
-    elif type(var) == int: v = (var, var)
-    else: v = var
-    x, y = np.mgrid[-s[0]:s[0]+1, -s[1]:s[1]+1]
-    g = np.exp(-(x**2/(2*float(v[0]))+y**2/(2*float(v[1]))))
-    return g / g.sum()
-
-def _gaussian_blur_image(imagesize, kernelsize, kernelvariance, position):
-    pos = position
-    ims = imagesize
-    ks = kernelsize
-    kernel = _gauss_kernel(ks, kernelvariance)
-    # generate a padded image
-    im = np.zeros((ims[0]+ks-1, ims[1]+ks-1))
-    im[pos[0]:pos[0]+ks, pos[1]:pos[1]+ks] = kernel
-    # remove padding
-    im = im[int(ks/2):-int(ks/2), int(ks/2):-int(ks/2)]
-    return im
-
-def _gaussian_blur_matrix(imagesize, kernelsize, kernelvariance):
-    dim = imagesize[0]*imagesize[1]
-    D = np.zeros((dim, dim))
-    for idx in range(dim):
-        pos = (int(idx/imagesize[0]), np.mod(idx, imagesize[0]))
-        D[:,idx] = _gaussian_blur_image(
-                imagesize, kernelsize, kernelvariance, pos
-                ).flatten()
-    return D
-
-
-
-"""
-DEPRECATED BUT STRUCTURALLY INTERESTING
-
-def gaussian_blur_matrix_sparse(dim, kernelsize, kernelvariance):
-    '''
-    
-    D is a symmetric matrix!
-
-    tested with : plt.imshow(D.toarray()[:100,:100])
-    '''
-    from scipy import sparse as sps
-    kernel = _gauss_kernel(kernelsize, kernelvariance).flatten()
-    kernel = kernel.flatten()
-    s = int(len(kernel)/2)
-    data = []
-    offsets = []
-    for i in range(len(kernel)):
-        data.append(np.ones(dim)*kernel[i])
-        offsets.append(s-i)
-    D = sps.dia_matrix((np.array(data), np.array(offsets)), shape=(dim, dim))
-    return D
-"""
 
 
 # SPARSE NON-NEGATIVE MATRIX FACTORIZATION CODE
 # -----------------------------------------------------------------------------
 
 class SMFF(object):
+    ''' An non-negative matrix factorization approach for calcium imaging data.
+
+    
+    A calcium imaging dataset Y, [Y] = p x T, is factorized according to
+
+    Y = AC + bf.T
+
+    with [A] = p x k, [C] = k x T, [b] = p x 1, [f] = T x 1
+    '''
 
     def __init__(self, *args, **kwargs):
+        self.logger = kwargs.pop('logger', logger)
+
         self.max_iter = kwargs.pop('max_iter', 100)
         self._step = 0
-        
+
+        self.alpha_sel = kwargs.pop('alpha_sel', 5.0)
+        self.alpha_fit = kwargs.pop('alpha_fit', 0.01)
+
+        self._model_params = ('A', 'C', 'f', 'b')
+        self._init_params = {} 
+        # check for initialization parameters
+        for ini in self.model_params:
+            self._init_params[ini] = kwargs.pop(ini, None)
+            if ini in kwargs:
+                self._init_params[ini] = kwargs[ini]
 
     def _stop(self):
         '''
         simple interation number based stop criterion for now
         '''
-        return self._step > self.max_iter
+
+        # alternatively we could calculate the residual and estimate whether it
+        # behaves noise style ..
+
+        return self._step >= self.max_iter
 
 
-    def init_model(self, ):
+    @property
+    def model_params(self):
+        return self._model_params
 
-        C, A, b, f = _init_model(Y, **kwargs)
+
+    def init_model(self, *args, **kwargs):
+        from . import _init_model
+        C, A, b, f = _init_model._init_model(*args, **kwargs)
+        self._init_params['C'] = C
+        self._init_params['A'] = A
+        self._init_params['b'] = b
+        self._init_params['f'] = f
 
 
     def fit_model(self, Y, **kwargs):
+
+        self.C = self._init_params['C'].copy()
+        self.A = self._init_params['A'].copy()
+        self.b = self._init_params['b'].copy()
+        self.f = self._init_params['f'].copy()
+
+        self.logger.info('Fitting SMFF to data Y.')
+        self.logger.debug('[Y] = '+str(Y.shape))
     
         while not self._stop():
-            C, f = self._update_C_f(C, A, b, f, Y)
-            A, b = self._update_A_b(C, A, b, f, Y)
-            
+            self.logger.info('iteration %s / %s ' % (self._step+1, self.max_iter))
             self._step += 1
+            # UPDATE A, b
+            self.A, self.b = self._update_A_b(self.C, self.A, self.b, self.f, Y)
+            # ROI component post processing
+            self.A = _morph_image_components(self.A)
+            # UPDATE C, f
+            self.C, self.f = self._update_C_f(self.C, self.A, self.b, self.f, Y)
 
 
-    @staticmethod
-    def _update_C_f(C, A, b, f, Y):
+    def _update_C_f(self, C, A, b, f, Y):
         '''
         minimize \sum (1 G c_j)
         subject to Gc_j >= 0, ||Y(i,:) - A(i,:)C - b(i)f.T || < \sigma_i * sqrt(T)
+
+        NOOOOOOT! (for now)
         '''
+        # recast the variables
+        V = Y.T
+        H = np.vstack([A.T, b])
+        W = np.vstack([C, f.T]).T
+        # multiplicative update
+        W_ = nmf.NMF_L0._update_W(V, H, W)
+        
+        C = W_[:, :-1].T
+        f = W_[:, -1].T 
 
-        return C, f
+        return C, f 
 
 
-    @staticmethod
-    def _update_A_b(C, A, b, f, Y):
+    def _update_A_b(self, C, A, b, f, Y, spl0=0.95):
         '''
         minimize || A ||_1
         subject to : A, b >= 0, ||Y(i,:) - A(i,:)C - b(i)f.T || < \sigma_i * sqrt(T)
         '''
+        A_, b_ = [], []
 
-        return A, b 
+        # calculate background data
+        if f.ndim == 1:
+            f = f[:, np.newaxis]
+        bg = np.dot(f, b).T
+
+        # we need to keep all alphas and all hs so that we can cut based on the
+        # alpha distribution post hoc -> need a better mechanism here! TODO
+        alphas = []
+        hs = []
+
+        # LOOP OVER ALL PIXELS (multiple times)
+        # TODO : !! parallelize !!
+
+        # 1. model selection loop : shrinkage on A only -----------------------
+        for pidx in range(A.shape[0]):
+            # i) subtract b from Y
+            R = np.array([(Y[pidx] - bg[pidx]),]).clip(TINY_POSITIVE_NUMBER, np.inf).T
+            # ii) use strong shrinkage to select components
+            ll = nmf.LARS(positive=True)
+            ll.fit(C.T, R)
+            alphas.append(ll.alphas_)
+            hs.append(ll.coef_path_)
+
+        # 2. calculate the cutoff ---------------------------------------------
+        alphs = np.concatenate(alphas)
+        numcomps = (1.-spl0)*Y.shape[0]*Y.shape[1]
+        cutoff = -np.percentile(-alphs, (1.-spl0)*100) 
+
+        # 3. calculate A and b
+        H = []
+        for n in range(len(hs)):
+            relevant_h = hs[n][:, np.where((alphas[n] - cutoff) <= 0)[0][0]]
+            comps = np.where(relevant_h)[0]
+            Cf = np.vstack([C[comps,:], f.T])
+            y = Y[n]
+            y = y[:, np.newaxis]
+
+            # coefficient estimation : selected ROIs including b
+            ll = nmf.LARS(positive=True)
+            ll.fit(Cf.T, Y[n], return_path=False, alpha_min=self.alpha_fit)
+
+            # append the coeffs
+            a = np.zeros(A.shape[1])
+            a[comps] = ll.coef_[:-1]
+            A_.append(a)
+            b_.append(ll.coef_[-1])
+
+        A = np.array(A_)
+        b = np.array(b_)
+        b = b[:, np.newaxis] # return a 2 dimensional array
+
+        return A, b.T
+
+
+def _morph_image_components(H):
+    from skimage import morphology
+    m, k = H.shape
+    w = int(np.sqrt(m))
+    imshape = (w,w)
+    for i in range(k):
+        H[:, i] = morph_close_component(H[:,i], imshape) 
+    return H
+
+
+def morph_close_component(a, imshape):
+    from skimage import morphology
+    amorph = morphology.closing(a.reshape(imshape[0], imshape[1]))
+    return amorph.flatten()
+
+
+def _lars(W, v, alpha):
+    ll = linear_model.LassoLars(fit_intercept=False, positive=True, alpha=alpha)
+    ll.fit(W, v)
+    H = np.array(ll.coef_)
+    return H, ll.intercept_
+
+def _linear_regression(W, v):
+    lr = linear_model.LinearRegression(fit_intercept=False)
+    lr.fit(W, v)
+    H = np.array(lr.coef_)
+    return H
