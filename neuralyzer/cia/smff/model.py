1470e742a97198837f11ca5417b0ad3497fb0ca1
mhg <michigraber@gmail.com>
2015-09-02 14:34:32 +0900

    joblib import through scikit-learn

--- neuralyzer/cia/smff/model.py
+++ neuralyzer/cia/smff/model.py
@@ -22,7 +22,7 @@ logger = log.get_logger()
 
 
 try:
-    from joblib import Parallel, delayed
+    from sklearn.externals.joblib import Parallel, delayed
     N_JOBS = -1
 except:
     print 'joblib could not be imported. NO PARALLEL JOB EXECUTION!'
