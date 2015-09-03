9b5e4c58fa31520702f5e8d8b872d2177e7dbfc7
mhg <michigraber@gmail.com>
2015-09-03 19:43:33 +0900

    jupyter notebook dev container

---
+++ docker/jupyter_dev/docker_run.sh
@@ -0,0 +1,1 @@
+docker run -d -p 443:8888 -v /Users/michael/coding/RIKEN/notebooks/:/workspace/notebooks -v /Users/michael/coding/RIKEN/data/:/data -e PASSWORD=michael michigraber/neuralyzer:jupyter_dev
