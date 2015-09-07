2de5b5f7059edfb32374837ec6de614337b43174
mhg <michigraber@gmail.com>
2015-09-07 18:24:25 +0900

    docker_dev container removed

--- docker/jupyter_dev/docker_run.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -p 443:8888 -v /Users/michael/coding/RIKEN/notebooks/:/workspace/notebooks -v /Users/michael/coding/RIKEN/data/:/data -e PASSWORD=michael michigraber/neuralyzer:jupyter_dev
